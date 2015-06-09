4819b528a057073e07ecfb38fdf306243532d185
lienvdsteen <lienvandensteen@gmail.com>
2015-06-09 22:12:23 +0200

    feature(restaurant): don't use integer for phone_number

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 20150609185307) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
-    t.text     "address"
-    t.integer  "phone_number"
+    t.string   "address"
+    t.string   "phone_number"
     t.string   "category"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
