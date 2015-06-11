0acba3653d25ee47f3215d5774a2f6dedef802bf
lienvdsteen <lienvandensteen@gmail.com>
2015-06-11 08:53:55 +0200

    feature(restaurant-image): add image to model and show when creating

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,7 @@ class Restaurant < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
 
   validates :address, presence: true
-  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false }, presence: true
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian", "mexican", "fast food", "fondue"], allow_nil: false }, presence: true
   validates :name, uniqueness: true, presence: true
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,22 @@
 Restaurant.destroy_all
 restaurants_attributes = [
   {
-    name:         "Epicure au Bristol",
-    address:      "112 rue du Fg St-Honoré 75008 Paris",
-    category:     "chinese"
+    name:         "Chipotle",
+    address:      "Las Tiendas Village 2895 S Alma School Rd Chandler, AZ",
+    category:     "mexican",
+    image_url:    "http://emilyadamsonfire.com/home/wp-content/uploads/2011/03/free-chipotle.jpg"
   },
   {
-    name:         "La truffière",
-    address:      "4 rue Blainville 75005 Paris",
-    category:     "chinese"
+    name:         "In & Out",
+    address:      "2790 W. Chandler Blvd. Chandler, AZ",
+    category:     "fast food",
+    image_url:    "http://i0.wp.com/blackchristiannews.com/go/wp-content/uploads/2015/04/In-N-Out.jpg?resize=500%2C250"
   },
   {
-    name:         "Le pré catelan",
-    address:      "route de Suresnes 75016 Paris",
-    category:     "chinese"
+    name:         "Melting Pot",
+    address:      "8260 N Hayden Rd. Scottsdale, AZ 85258",
+    category:     "fondue",
+    image_url:    "http://roamthere.com/wp-content/uploads/2013/04/fondue-500x250.jpg"
   }
 ]
 restaurants_attributes.each { |params| Restaurant.create!(params) }
